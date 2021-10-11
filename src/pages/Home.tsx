af8c9e8260534dab3973544e06b4d7c75f395dad
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-10-11 11:45:53 -0700

    add SearchBar to Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonSearchbar, IonFooter } from '@ionic/react';
 import SpaceXCall from '../components/SpaceXCall';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>SpaceX Missions</IonTitle>
+          <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} animated showCancelButton="focus" placeholder="Search missions by name"/>
+          <IonFooter>
+        <IonToolbar>
+          Search Text: {searchText ?? '(none)'}
+        </IonToolbar>
+      </IonFooter>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Test</IonTitle>
           </IonToolbar>
-        </IonHeader>
-        <SpaceXCall />
+        </IonHeader> */}
+        <IonList>
+          <SpaceXCall />
+        </IonList>
       </IonContent>
     </IonPage>
   );
