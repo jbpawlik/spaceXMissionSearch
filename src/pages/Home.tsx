f89b4fa4d56874e3db5c24707cc44009334ebe89
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-10-10 18:08:39 -0700

    add SpaceXCall to index.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import SpaceXCall from '../components/SpaceXCall';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -16,7 +17,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <SpaceXCall />
       </IonContent>
     </IonPage>
   );
